0d550cc01677b762036b8158b7e38f1fdb41ace9
gannyee <gannyee@users.noreply.github.com>
2015-04-01 01:25:15 +0800

    Create numbersOfCommenDivision.py
    #给你两个正整数a,b,  输出它们公约数的个数。

---
+++ numbersOfCommenDivision.py
@@ -0,0 +1,19 @@
+#给你两个正整数a,b,  输出它们公约数的个数。
+
+def divisionCount(s):
+    division = []
+    for i in range(1,s + 1):
+        if s % i == 0:
+            division.append(i)
+    return division
+    
+def CommenDivision(a,b):
+    count = 0
+    for i in a:
+        for j in b:
+            if i == j:
+                count += 1
+    return count
+
+
+print CommenDivision(divisionCount(a),divisionCount(b))
