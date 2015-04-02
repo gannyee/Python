32d61a8ed459d9713fcc54c1d8594aa7a64bde0e
gannyee <gannyee@users.noreply.github.com>
2015-04-02 22:05:33 +0800

    Update Fibonacci.py

--- Fibonacci.py
+++ Fibonacci.py
@@ -9,3 +9,25 @@ class Fib(object):
 
 f = Fib()
 print f(10)
+
+#Solution 2:
+class Fib(object):
+
+    def __init__(self, num):
+        a,b,L = 0,1,[]
+        for n in range(num):
+            L.append(a)
+            a, b = b, a + b
+            
+        self.numbers = L
+
+    def __str__(self):
+        return str(self.numbers)
+        
+    __repr__ = __str__
+    
+    def __len__(self):
+        return len(self.numbers)
+f = Fib(100)
+print f
+print len(f)
